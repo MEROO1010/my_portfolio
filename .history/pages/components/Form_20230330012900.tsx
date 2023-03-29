64494d7ae47bd36c72f3248770510f0df0b428d5
unknown <meroo1995saggaf@outlook.com>
2023-03-30 01:51:19 +0300

    updated and add new staff

---
+++ .history/pages/components/Form_20230330012900.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div>
+        <input type="text" className=" relative  px-80 mx-80 bottom-10 left-44 h-8 w-0"/><br/>
+        <input type="text" className=" relative  px-80 mx-80 top-10 left-44 h-8 w-0"/><br/>
+        <button>Send</button>
+        
+    </div>
+  )
+}
+
+export default Form
--- .history/pages/components/Form_20230330014932.tsx
+++ .history/pages/components/Form_20230330014932.tsx
@@ -5,7 +5,7 @@ const Form = () => {
     <div>
         <input type="text" className=" relative  px-80 mx-80 bottom-24 left-44 h-14 w-0 rounded-2xl"/><br/>
         <input type="text" className=" relative  px-80 mx-80 bottom-8 left-44 h-14 w-0 rounded-2xl"/><br/>
-        <textarea name="comment" rows={4} cols={50}  minLength={10} form="usrform" className=" relative  px-80 mx-80 top-8 left-44 w-0 h-12 rounded-xl">Message</textarea>
+        <textarea name="comment" rows={4} cols={50}  minLength={10} form="usrform" className=" relative  px-80 mx-80 top-8 left-44 w-0 h-12 rounded-xl"/>
         <button type="submit" className="relative px-80 mx-80 top-20 left-44 h-14 w-10 font-raleway text-[24px] font-semibold text-[#FFFCF6]  bg-[#B8BFD6] rounded-2xl">Send</button>
         
     </div>
