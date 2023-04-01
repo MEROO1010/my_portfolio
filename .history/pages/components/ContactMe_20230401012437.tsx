ebac182d6525868d1beb714d190bbd22ec7faabe
unknown <meroo1995saggaf@outlook.com>
2023-04-01 03:18:33 +0300

    update contact me file

--- .history/pages/components/ContactMe_20230401012437.tsx
+++ .history/pages/components/ContactMe_20230401012437.tsx
@@ -4,7 +4,7 @@ import Form from './Form'
 
 const ContactMe = () => {
   return (
-    <div className=" relative py-80   bottom-40   bg-[#19335A]">
+    <div className=" absolute py-80  bottom-40   bg-[#19335A]">
           <div className="h-100 ">
         <div>
           <h1 className= " absolute py-20 px-80 mx-10 top-0 left-80 font-raleway font-bold  text-[50px] text-[#FFFCF6]">Contact Me</h1>
