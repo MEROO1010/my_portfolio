29f3e6981b0a235015d1415d276facc5feb39c83
unknown <meroo1995saggaf@outlook.com>
2023-04-04 02:25:39 +0300

    update FOrm file

--- .history/pages/components/Form_20230403235513.tsx
+++ .history/pages/components/Form_20230403235513.tsx
@@ -5,7 +5,7 @@ const Form = () => {
     <div>
         <input type="text" placeholder='Your Name' className=" relative  px-80 mx-80 bottom-24 left-44 h-14 w-0 rounded-full"/><br/>
         <input type="text" className=" relative  px-80 mx-80 bottom-8 left-44 h-14 w-0 rounded-full"/><br/>
-        <textarea  className=" relative  px-80 mx-80 top-8 left-44   overflow-auto  w-5 h-96  text-m text-gray-900 bg-gray-50  border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 rounded-3xl ">Message</textarea>
+        <textarea name="comment" className=" relative  px-80 mx-80 top-8 left-44 block p-2.5 w-5 h-96  text-sm text-gray-900 bg-gray-50  border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 rounded-3xl ">Message</textarea>
         <button type="submit" className="relative px-80 mx-80 top-20 left-44 h-14 w-10 font-raleway text-[24px] font-semibold text-[#FFFCF6]  bg-[#B8BFD6] rounded-full">Send</button>
         
     </div>
